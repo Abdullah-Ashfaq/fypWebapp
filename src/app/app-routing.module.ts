030e2863a16fcc43e04ea47282c30e2291bc08e1
Abdullah <mughalian911@gmail.com>
2022-03-27 18:29:27 +0500

    pushing code!!!!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeCompComponent } from './home-comp/home-comp.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeCompComponent,
+    pathMatch:'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeCompComponent } from './home-comp/home-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeCompComponent
   ],
   imports: [
     BrowserModule,
