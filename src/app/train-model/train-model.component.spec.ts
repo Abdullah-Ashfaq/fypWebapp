3a92f6ce979c0babbb7d7dc840dd108ac7383c86
Abdullah <mughalian911@gmail.com>
2022-04-01 22:16:45 +0500

    push

--- src/app/train-model/train-model.component.spec.ts
+++ src/app/train-model/train-model.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TrainModelComponent } from './train-model.component';
 
+
 describe('TrainModelComponent', () => {
   let component: TrainModelComponent;
   let fixture: ComponentFixture<TrainModelComponent>;
